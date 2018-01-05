c00a61a63df3896b29b6e9bc8991fee5d6cf19b4
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 03:33:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length ==1
+    return array.join
+  elsif array.length ==2
+    return array.join(" and ")
+  else
+    last = array.pop
+    ammend = ", and #{last}"
+    return array[0..(array.length-1)].join(", ") + ammend
+  end
 end
